a9bb703022164cf75dba1e303cae943419394f63
Nayotama Pradipta <n4y0t4m4@gmail.com>
2024-07-09 13:18:46 +0700

    improve css

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/home.css" type="text/css">
     <title>Binotify App</title>
 
 </head>
 <body>
-    <h1>Binotify</h1>
+    <div class="header"> 
+        <h1>Binotify</h1>
+        <a href="login.php" class="login-button">Login</a>
+    </div>
 </body>
 </html>
