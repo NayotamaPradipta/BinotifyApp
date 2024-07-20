21e1739221c37b07fd1d17dcb9a78eaae9809202
NayotamaPradipta <n4y0t4m4@gmail.com>
2024-07-20 15:16:36 +0700

    feat: add song css

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/home.css" type="text/css">
+    <link rel="stylesheet" href="public/css/navbar.css" type="text/css">
+    <link rel="stylesheet" href="public/css/songs.css" type="text/css">
     <title>Binotify App</title>
 
 </head>
