eb09b263e0ce3781199d1ac6685f4866001f261b
NayotamaPradipta <n4y0t4m4@gmail.com>
2024-07-24 20:58:57 +0700

    feat: add song detail css

--- song_detail.php
+++ song_detail.php
@@ -23,26 +23,29 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo htmlspecialchars($row['judul']); ?></title>
+    <link rel="stylesheet" href="public/css/song_detail.css" type="text/css">
 </head>
 <body>
     <?php 
-        include 'navbar.php';
+        include 'navbar.php'; 
     ?>
-    <div class="song-detail">
-        <img src="<?php echo htmlspecialchars($row['image_path']); ?>" alt="Album cover">
-        <div>
-            <h2><?php echo htmlspecialchars($row['judul']); ?></h2>
-            <div class="song-details">
-                <span><?php echo htmlspecialchars($row['penyanyi']); ?></span>
-                <span>&#8226;</span> 
-                <span><?php echo htmlspecialchars(substr($row['tanggal_terbit'], 0, 4)); ?></span>
-                <span>&#8226;</span> 
-                <span><?php echo htmlspecialchars($row['genre']); ?></span>
+    <div class="song-detail-wrapper">
+        <div class="song-detail">
+            <img src="<?php echo htmlspecialchars($row['image_path']); ?>" alt="Album cover">
+            <div>
+                <h2><?php echo htmlspecialchars($row['judul']); ?></h2>
+                <div class="song-details">
+                    <span><?php echo htmlspecialchars($row['penyanyi']); ?></span>
+                    <span>&#8226;</span> 
+                    <span><?php echo htmlspecialchars(substr($row['tanggal_terbit'], 0, 4)); ?></span>
+                    <span>&#8226;</span> 
+                    <span><?php echo htmlspecialchars($row['genre']); ?></span>
+                </div>
+                <audio controls>
+                    <source src="<?php echo htmlspecialchars($row['audio_path']); ?>" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
             </div>
-            <audio controls>
-                <source src="<?php echo htmlspecialchars($row['audio_path']); ?>" type="audio/mpeg">
-                Your browser does not support the audio element.
-            </audio>
         </div>
     </div>
 </body>
