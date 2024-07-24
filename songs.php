7aebf512fe9fa50829e3a62c763640127162e7d5
NayotamaPradipta <n4y0t4m4@gmail.com>
2024-07-24 20:20:19 +0700

    feat: song detail page

--- songs.php
+++ songs.php
@@ -6,17 +6,18 @@
 
     while ($row = $stmt->fetch()){
         echo '<div class="song">';
-        echo '<img src="' . htmlspecialchars($row['image_path']) . '" alt="Album cover">';
-        echo '<div>';
-        echo '<h2>' . htmlspecialchars($row['judul']) . '</h2>';
-        echo '<div class="song-details">';
-        echo '<span>' . htmlspecialchars($row['penyanyi']) . '</span>';
-        echo '<span>&#8226;</span>'; 
-        echo '<span>' . htmlspecialchars(substr($row['tanggal_terbit'], 0, 4)) . '</span>';
-        echo '<span>&#8226;</span>'; 
-        echo '<span>' . htmlspecialchars($row['genre']) . '</span>';
-        echo '</div>';
-        echo '</div>';
+            echo '<a href="song_detail.php?id=' . htmlspecialchars($row['song_id']) . '">';
+            echo '<img src="' . htmlspecialchars($row['image_path']) . '" alt="Album cover">';
+            echo '<div>';
+                echo '<h2>' . htmlspecialchars($row['judul']) . '</h2>';
+                    echo '<div class="song-details">';
+                        echo '<span>' . htmlspecialchars($row['penyanyi']) . '</span>';
+                        echo '<span>&#8226;</span>'; 
+                        echo '<span>' . htmlspecialchars(substr($row['tanggal_terbit'], 0, 4)) . '</span>';
+                        echo '<span>&#8226;</span>'; 
+                        echo '<span>' . htmlspecialchars($row['genre']) . '</span>';
+                    echo '</div>';
+            echo '</div>';
         echo '</div>';
     }
 ?>
