675b0db6e014bc5fa46aa436b806c44b74d2fc4e
NayotamaPradipta <n4y0t4m4@gmail.com>
2024-08-03 15:03:22 +0700

    feat: album detail css

--- album_detail.php
+++ album_detail.php
@@ -16,6 +16,17 @@
         echo 'Album not found';
         exit;
     }
+
+    // Fetch songs 
+
+    $stmt = $pdo->prepare('SELECT * FROM song WHERE album_id = :id');
+    $stmt->execute(['id' => $id]);
+    $songs = $stmt->fetchAll();
+
+    if (!$songs) { 
+        echo 'Songs not found';
+        exit;
+    }
 ?> 
 
 <!DOCTYPE html>
@@ -23,6 +34,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo htmlspecialchars($row['judul']); ?></title>
+    <linK rel="stylesheet" href="public/css/album_detail.css" type="text/css">
 </head>
 
 <body>
@@ -35,15 +47,25 @@
             <div>
                 <h2><?php echo htmlspecialchars($row['judul']); ?></h2>
                 <div class="album-details">
-                    <span><?php echo htmlspecialchars($row['penyanyi']); ?></span>
-                    <!-- TODO: 
-                        1. Count total duration
-                        2. List of Songs    
-                    --> 
+                    <span>ALBUM BY <?php echo htmlspecialchars($row['penyanyi']); ?></span>
                 </div>
             </div>
+            <h2>Songs</h2>
+            <div class="song-container">
+                <?php foreach ($songs as $song): ?>
+                    <div class="song">
+                        <a href="song_detail.php?id=<?php echo htmlspecialchars($song['song_id']); ?>">
+                            <div>
+                                <span class="song-title"><?php echo htmlspecialchars($song['judul']); ?></span>
+                                <div class="song-details">
+                                    <span><?php echo htmlspecialchars($song['penyanyi']); ?></span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
-    
 
     </div>
 </body>
